12d296aba6d42f6b774a46faafe4bd04da3ab7c5
ptlnik13 <nikeshvpatel@gmail.com>
2023-09-08 15:24:35 -0400

    Building the AppLayout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header.jsx";
+import CartOverview from "../features/cart/CartOverview.jsx";
+import {Outlet} from "react-router-dom";
+
+
+function AppLayout() {
+    return (
+        <div>
+            <Header/>
+            <main>
+                <Outlet/>
+            </main>
+            <CartOverview/>
+        </div>
+    );
+}
+
+export default AppLayout;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+
+function Header() {
+    return (
+        <header>
+            <Link to='/'>Fast React Pizza Co.</Link>
+            Nikesh
+        </header>
+    );
+}
+
+export default Header;
